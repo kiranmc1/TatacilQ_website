2521b6ce5b6e6ec01e8d9e4cf12406b38d52a1ed
kiranmc1 <kiranmc314@gmail.com>
2026-06-21 08:44:44 +0530

    initial react push

---
+++ Frontend/tatacaliq/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="app-shell">
+      <Header />
+      <main className="app-main">
+        <h1>Welcome to Tata Caliq</h1>
+        <p>Your trusted place to manage and explore services.</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/tatacaliq/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+const navItems = ['Home', 'About', 'Services', 'Contact']
+
+function Header() {
+  return (
+   
+   <>
+    <div className='logo-handler'>
+        <div className='logo'></div>
+        <div>header2</div>
+        <div>header3</div>
+    </div>   
+   </>
+  )
+}
+
+export default Header
